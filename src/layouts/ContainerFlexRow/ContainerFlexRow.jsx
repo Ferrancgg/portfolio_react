d91847869b6be027637b3a9eedf4704a75172c6b
Ferrancgg <ferrancgg@gmail.com>
2023-10-19 18:01:09 +0200

    primeros pasos card proyect container, falta mirar posiciones relativas,tb hhecho el componente card

---
+++ src/layouts/ContainerFlexRow/ContainerFlexRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContainerFlexRow = (children) => {
+  return (
+    <div className='container-flex-row'>{children}</div>
+  )
+}
+
+export default ContainerFlexRow
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import ProyectCard from "../../components/ProyectCard/ProyectCard";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      {/* <div className="proyect-card">
+        <div className="background-border">
+          <div className="container-information">
+            <div className="text-container">
+              <h2>nombre</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+                ipsa nulla. Voluptatum corrupti, praesentium nostrum quos sint
+                omnis. Voluptatum voluptates ipsa debitis nihil nobis dolor
+                distinctio voluptas tenetur ex autem.
+              </p>
+            </div>
+            <div className="img-container">
+                <img src="https://res.cloudinary.com/di49fnkc8/image/upload/v1679929496/web%20fotos/foto_bn19_gfuaqp.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <ProyectCard/>
+    </div>
+  );
+};
+
+export default Home;
